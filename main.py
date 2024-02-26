215f6dc0a1b62566352d761c8ee693a2da60c874
Виктор Антропов <antropovvic1997@yandex.ru>
2024-02-26 20:11:34 +0300

    Work

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import requests
 import csv
 import telebot
 import schedule
-import time
-
 from bs4 import BeautifulSoup
 from pathlib import Path
 import datetime as dt
@@ -12,14 +10,14 @@ import datetime as dt
 BASE_DIR = Path(__file__).parent
 DATETIME_FORMAT = '%Y-%m-%d'
 now = dt.datetime.now()
-format_time = now.strftime(DATETIME_FORMAT)
+FORMAT_TIME = now.strftime(DATETIME_FORMAT)
 bot = telebot.TeleBot(token='5865043978:AAEawcnDACERjDdwuOQ9ovlq69hiPxHdhdY')
 
 
-def csv_output(results, result_list):
+def csv_output(result_list):
     dir = BASE_DIR / 'results'
     file_path = dir / '2024-02-26.csv'
-
+   
     with open(file_path, 'a', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile, dialect='unix')
         writer.writerow(result_list)
@@ -30,9 +28,6 @@ def csv_output(results, result_list):
 def parser_the_bank():
     a = []
     b = []
-    now = dt.datetime.now()
-    format_time = now.strftime(DATETIME_FORMAT)
-    results = ['Дата', 'Цель по инфляции', 'Инфляция', 'Ключевая ставка', 'Ставка RUOINIA', 'CNY', 'USD', 'EUR']
     response = requests.get('https://cbr.ru/')
     soup = BeautifulSoup(response.text, features='lxml')
     div_tag = soup.find('div', attrs={'class': 'home-main_aside'})
@@ -46,9 +41,9 @@ def parser_the_bank():
         a.append(i.text.strip())
 
     result_list = b + a[1::2]
-    result_list.insert(0, format_time)
+    result_list.insert(0, FORMAT_TIME)
     print(result_list)
-    csv_output(results, result_list)
+    csv_output(result_list)
 
 
 def send_telegram_message(file_path):
@@ -57,16 +52,12 @@ def send_telegram_message(file_path):
         bot.send_document(chat_id, f)
 
 
-# schedule.every(10).seconds.do(parser_the_bank)
-
-# while True:
-#     schedule.run_pending()
-
-
 def main():
-    print('Парсер запустили')
-    # schedule.every(10).seconds.do(parser_the_bank)
-    # while True:
-    #     schedule.run_pending()
-    if __name__ == '__main__':
-        main()
+    print('Парсер запустился')
+    schedule.every().monday.do(parser_the_bank)
+    while True:
+        schedule.run_pending()
+
+
+if __name__ == '__main__':
+    main()
