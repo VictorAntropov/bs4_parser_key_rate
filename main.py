0e34dad78269e1d509db203b686bc1c82d488bb4
Виктор Антропов <antropovvic1997@yandex.ru>
2024-02-26 20:50:46 +0300

    Check

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ bot = telebot.TeleBot(token='5865043978:AAEawcnDACERjDdwuOQ9ovlq69hiPxHdhdY')
 def csv_output(result_list):
     dir = BASE_DIR / 'results'
     file_path = dir / '2024-02-26.csv'
-   
+
     with open(file_path, 'a', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile, dialect='unix')
         writer.writerow(result_list)
@@ -26,24 +26,17 @@ def csv_output(result_list):
 
 
 def parser_the_bank():
-    a = []
-    b = []
     response = requests.get('https://cbr.ru/')
     soup = BeautifulSoup(response.text, features='lxml')
     div_tag = soup.find('div', attrs={'class': 'home-main_aside'})
-    div_tag_2 = div_tag.find_all('div', class_='main-indicator_value')
-    div_tag_3 = div_tag.find_all('div', attrs={'class': 'col-md-2 col-xs-9 _right mono-num'})
+    percent_value = div_tag.find_all('div', class_='main-indicator_value')
+    value = div_tag.find_all('div', attrs={'class': 'col-md-2 col-xs-9 _right mono-num'})
+    percent_value = [(item.text.strip()) for item in percent_value]
+    result_value = [(item.text.strip()) for item in value]
+    result = percent_value + result_value[1::2]
+    result.insert(0, FORMAT_TIME)
 
-    for i in div_tag_2:
-        b.append(i.text.strip())
-
-    for i in div_tag_3:
-        a.append(i.text.strip())
-
-    result_list = b + a[1::2]
-    result_list.insert(0, FORMAT_TIME)
-    print(result_list)
-    csv_output(result_list)
+    return csv_output(result)
 
 
 def send_telegram_message(file_path):
@@ -54,6 +47,7 @@ def send_telegram_message(file_path):
 
 def main():
     print('Парсер запустился')
+    # print(parser_the_bank())
     schedule.every().monday.do(parser_the_bank)
     while True:
         schedule.run_pending()
